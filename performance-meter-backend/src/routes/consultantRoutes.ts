cd3bfd0667b74b675b1cf85a17dde415fb682ac2
maarten7888 <maarten@maarten-jansen.nl>
2025-03-18 19:50:08 +0100

    Toegevoegde debug logging aan consultant routes

--- performance-meter-backend/src/routes/consultantRoutes.ts
+++ performance-meter-backend/src/routes/consultantRoutes.ts
@@ -5,11 +5,24 @@ import { authenticateToken } from '../middleware/auth';
 const router = express.Router();
 const consultantController = new ConsultantController();
 
+// Debug middleware voor consultant routes
+router.use((req, res, next) => {
+  console.log(`[ConsultantRoutes] ${req.method} ${req.url}`);
+  next();
+});
+
 // Bescherm alle routes met authenticatie
 router.use(authenticateToken as RequestHandler);
 
 // Consultant routes
-router.get('/', consultantController.getAllConsultants);
-router.put('/:id/target', consultantController.updateYearTarget);
+router.get('/', (req, res, next) => {
+  console.log('[ConsultantRoutes] GET / - Fetching all consultants');
+  consultantController.getAllConsultants(req, res).catch(next);
+});
+
+router.put('/:id/target', (req, res, next) => {
+  console.log(`[ConsultantRoutes] PUT /${req.params.id}/target - Updating year target`);
+  consultantController.updateYearTarget(req, res).catch(next);
+});
 
 export default router; 
